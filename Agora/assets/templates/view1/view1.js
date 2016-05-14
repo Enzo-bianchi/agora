f40e83ca3be9d191bc7ceaf9d56e49f9eef04d98
Enzo BIANCHI <enzo.bianchi@free.fr>
2016-05-14 16:59:51 +0200

    deuxième commit

--- Agora/assets/templates/view1/view1.js
+++ Agora/assets/templates/view1/view1.js
@@ -3,12 +3,27 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
+    $routeProvider.when('/view1', {
+        templateUrl: '/templates/view1/view1.html',
+        controller: 'View1Ctrl'
+    });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', function($scope) {
+    $scope.$parent.title = "Fil des idées";
+    var dialog = document.querySelector('dialog');
+    var showDialogButton = document.querySelector('#show-dialog');
+    if (!dialog.showModal) {
+        dialogPolyfill.registerDialog(dialog);
+    }
+    showDialogButton.addEventListener('click', function() {
+        dialog.showModal();
+    });
+    dialog.querySelector('.close').addEventListener('click', function() {
+        dialog.close();
+    });
+    document.querySelector('#p1').addEventListener('mdl-componentupgraded', function() {
+        this.MaterialProgress.setProgress(44);
+    });
 
 }]);
--- Agora/assets/templates/view2/view2.js
+++ Agora/assets/templates/view2/view2.js
@@ -4,11 +4,11 @@ angular.module('myApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
+    templateUrl: '/templates/view2/view2.html',
     controller: 'View2Ctrl'
   });
 }])
 
-.controller('View2Ctrl', [function() {
-
+.controller('View2Ctrl', ['$scope',function($scope) {
+	$scope.$parent.title = "Vue 2";
 }]);
